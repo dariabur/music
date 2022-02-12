35ea635715ff2fb158eccfb1160ba26850e73fd0
Дария Бурлова <dariaburlova@gmail.com>
2022-02-13 00:38:04 +0300

    1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,10 +9,10 @@ fun main() {
     if (previousPurchase >= 1_001 && previousPurchase <= 10_000) {
         result = itemPrice * itemCount - 100.00
     }
-    else if (previousPurchase >= 10_001) {
-        result = itemPrice * itemCount * 0.95
-        preResult = (itemPrice * itemCount).toDouble()
-    }
+        else if (previousPurchase >= 10_001) {
+            result = itemPrice * itemCount * 0.95
+            preResult = (itemPrice * itemCount).toDouble()
+        }
     if (musicLover)
        result = itemPrice * itemCount * 0.95 * 0.99
      println("Сумма покупки без скидки: $preResult")
