5e3336f0c43603f5700ae2fb1a87ac0c442fee31
Дария Бурлова <dariaburlova@gmail.com>
2022-02-13 20:40:52 +0300

    1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,18 +3,23 @@ fun main() {
     val previousPurchase = 10_001
     val itemPrice = 100
     val itemCount = 11
-    var result:Double = 0.0
-    var preResult:Double = 0.0
+    var result: Double = 0.0
+    var preResult: Double = 0.0
+    var result1: Double = 0.0
 
     if (previousPurchase >= 1_001 && previousPurchase <= 10_000) {
-        result = itemPrice * itemCount - 100.00
+        result1 = itemPrice * itemCount - 100.00
+    } else if (previousPurchase >= 10_001) {
+        result1 = itemPrice * itemCount * 0.95
+    } else if (previousPurchase <= 1_000) {
+        result1 = (itemPrice * itemCount).toDouble()
     }
-        else if (previousPurchase >= 10_001) {
-            result = itemPrice * itemCount * 0.95
-            preResult = (itemPrice * itemCount).toDouble()
-        }
-    if (musicLover)
-       result = itemPrice * itemCount * 0.95 * 0.99
+    result = if (musicLover) {
+        result1 * 0.99
+    } else {
+        result1
+    }
+     preResult = (itemPrice * itemCount).toDouble()
      println("Сумма покупки без скидки: $preResult")
      println("Итоговая сумма покупки: $result")
 }
